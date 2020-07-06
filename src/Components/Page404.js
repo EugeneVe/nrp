e0d54f6eb2dd0a12760f80df52daff82a4026e85
EugeneVe <eugene.veprytskyi@gmail.com>
2020-07-06 14:22:25 +0300

    fixed few styles

--- src/Components/Page404.js
+++ src/Components/Page404.js
@@ -1,27 +1,30 @@
-import React from 'react'
+import React from "react"
 import "../sass/style.scss"
-import Blurswitch from './Blurswitch'
+import Blurswitch from "./Blurswitch"
 import { Link } from "react-router-dom"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFeatherAlt } from '@fortawesome/free-solid-svg-icons'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFeatherAlt } from "@fortawesome/free-solid-svg-icons"
 
 const Page404 = () => {
-    return (
-        <div className="MainContent">
-            <div className="containerWarp contentInner">
-                <div className="anyPage">
-                    <div className="empty">
-                        <h2>404 OOPS!</h2>
-                        <p className="faFeatherAlt"><FontAwesomeIcon icon={faFeatherAlt} /></p>
-                        <p>Not Found <br /> Page you requested not found</p>
-                        <Link to="/" className="navUnderlineNone navList"><button className="sendButton switch-on">Home</button></Link>
-                    </div>
-                </div>
-                <Blurswitch />
-            </div >
-        </div >
-    )
+  return (
+    <div className="MainContent">
+      <div className="home">
+        <div className="containerWarp contentInner textPcentered">
+          <h2>404 OOPS!</h2>
+          <p className="faFeatherAlt">
+            <FontAwesomeIcon icon={faFeatherAlt} />
+          </p>
+          <p>
+            Not Found <br /> Page you requested not found
+          </p>
+          <Link to="/" className="navUnderlineNone navList">
+            <button className="sendButton switch-on">Home</button>
+          </Link>
+          <Blurswitch />
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Page404;
+export default Page404
